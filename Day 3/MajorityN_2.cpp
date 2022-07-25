c4863d1ca1fafb8bc18caadc9d2ef23261331181
pankajsingh016 <pankrj123@gmail.com>
2022-07-25 08:53:39 +0530

    .

---
+++ Day 3/MajorityN_2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int N;
+        cin>>N;
+
+        vector<int>arr(N);
+        for(int i=0;i<N;i++)
+        {
+            cin>>arr[i];
+        }
+
+        
+    }
+    return 0;
+}
